389ec5f12614012ccafbde243497ed4f6ec6054b
fitsum <belay.fitsum@gmail.com>
2025-09-09 17:10:33 +0300

    merging branch to master and editing api

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Nice try');
+    res.send('Build and Deploy to EKS Project');
 });
 
 app.listen(8080, () => {
