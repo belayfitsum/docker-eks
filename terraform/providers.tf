5d69c5d6f6c7044633539534a6da06d7e7f33b4d
fitsum <belay.fitsum@gmail.com>
2025-09-10 23:14:09 +0300

    added s3 policy for terraform1.8

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -9,8 +9,8 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "my-api-test-buck"
-    key            = "infra.tfstate"
+    bucket         = "my-api-test-buck-cd"
+    key            = "infra.tfstate.cd"
     region         = "eu-central-1"
     encrypt        = true
     dynamodb_table = "simpleweb-api-tf-lock"
