4e82a40412e981cd609328f76ded47cc827c99c9
fitsum <belay.fitsum@gmail.com>
2025-09-10 21:04:58 +0300

    added s3 policy for terraform1.1

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -24,7 +24,7 @@ data "aws_iam_policy_document" "tf_backend" {
   statement {
     effect = "Allow"
     actions = [ "s3:GetObject", "s3:PutObject", "s3:DeleteObject" ]
-    resources = [ "arn:aws:s3:::${var.tf_state_bucket}/infra.tfstate/*" ]
+    resources = [ "arn:aws:s3:::${var.tf_state_bucket}/infra.tfstate" ]
   }
 
   statement {
