83e46da4eff01046a620933bb134f8a634007636
fitsum <belay.fitsum@gmail.com>
2025-09-12 10:32:04 +0300

    running tf with temporary assumed role

--- README.md
+++ README.md
@@ -2,19 +2,19 @@
 
 Build, test and deploy a small express application to Elastic kubernetes Service (EKS) while learning deployment workflows using github CICD. 
 
-# Project 
+### Project 
 
 I began by setting up an S3 backend for Terraform and provisioning an ECR repository as the initial step. I then added EKS cluster creation using the official AWS EKS module, which also installs the necessary dependencies for the cluster to run.
 
 However, I encountered issues during node creation since some resources needed to be in place before the module could complete successfully. To work around this, I explored an alternative approach. The original setup has been commented out and kept in the codebase for future development.
 
-# New setup
+### New setup
 
 Provisioned the EKS cluster using eksctl, which made the process much simpler. eksctl deploys AWS CloudFormation stacks that automatically create and configure all required resources, such as worker nodes, VPC, subnets, IAM roles, and permissions.
 
 While I had Administrator access to the deployment environment (AWS), I followed best practices by creating a dedicated CI/CD user with only the minimum required permissions instead of relying on admin credentials. This adds an extra layer of security and ensures least-privilege access. The CICD user has only needed privillages to run pipline jobs. 
 
-# Prerequisites
+### Prerequisites
 
 - AWS account and IAM admin access (for initial setup)
 
@@ -35,9 +35,8 @@ eksctl create cluster \
   --node-type t2.micro \
   --nodes 2
 
-- Save the file in the project and push it to github. The pipline triggered with push will walk through each job using a gihb hosted runners and perform each steps as dictated in the github workflows file. 
+### Tools/environments
 
-# Tools/environments
 - ECR- Docker images repository to hold the images built
 - Docker - for containerizng the express app
 - Kubernetes- to deployment environment for the containerized app 
@@ -56,13 +55,17 @@ eksctl create cluster \
 
 - GitHub Actions (CI/CD) then builds the Docker image, pushes it to ECR, and deploys it to the EKS cluster. 
 
-# Notes
+### Notes
 
 GitHub Actions now uses the dedicated CICD user to run pipeline jobs.
 When new permissions are required (for example, to access EC2, EKS, or other AWS services), switch temporarily to the admin user to set up those permissions.
 
 After the policies are updated, you can safely switch back to the CICD user, ensuring pipelines continue running with the principle of least privilege.
 
-# Future Improvments
+### Project cleanup
 
-- 
+- Terraform
+---terraform destroy\
+- Kubernetes
+---eksctl delete cluster --name XXX
+
--- terraform/infra.tf
+++ terraform/infra.tf
@@ -1,118 +1,118 @@
 
-# module "vpc" {
-#   source = "terraform-aws-modules/vpc/aws"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-#   name = "eks-vpc"
-#   cidr = "10.0.0.0/16"
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
 
-#   azs             = ["eu-central-1a", "eu-central-1b"]
-#   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-#   enable_nat_gateway = false
-#   enable_vpn_gateway = false
+  azs             = ["eu-central-1a", "eu-central-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
 
-#     # This setting is required for EKS nodes to receive a public IP address
-#   map_public_ip_on_launch = true
-
-
-#   tags = {
-#     "project" = "ci-cd-eks"
-#   }
-# }
-
-# # This creates the EKS cluster and a managed node group.
-# module "eks" {
-#   source  = "terraform-aws-modules/eks/aws"
-#   version = "21.1.5"
-
-#   endpoint_public_access  = true   # allow API access from internet
-#   endpoint_private_access = true 
-
-#   name  = "my-express-app-eks"
-#   kubernetes_version = "1.29"
-
-#   vpc_id                   = module.vpc.vpc_id
-#   subnet_ids               = module.vpc.public_subnets
-#   control_plane_subnet_ids = module.vpc.public_subnets
-
-#   # Configure the managed node group
-#   eks_managed_node_groups = {
-#     express_app_nodes = {
-#       min_size       = 1
-#       max_size       = 2 # Changed max_size to allow for scaling
-#       desired_size   = 1
-#       instance_types = ["t3.medium"]
-#     }
-#   }
-
-#   tags = {
-#     "project" = "ci-cd-eks"
-#   }
-# }
-
-# data "aws_eks_cluster" "this" {
-#   name = module.eks.cluster_name
-# }
-# data "aws_eks_cluster_auth" "this" {
-#   name = module.eks.cluster_name
-# }
+    # This setting is required for EKS nodes to receive a public IP address
+  map_public_ip_on_launch = true
 
 
-# provider "kubernetes" {
-#   host                   = module.eks.cluster_endpoint
-#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-#   token                  = data.aws_eks_cluster_auth.this.token
+  tags = {
+    "project" = "ci-cd-eks"
+  }
+}
 
-# }
+# This creates the EKS cluster and a managed node group.
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "21.1.5"
 
-# # Kubernetes Deployment for the Express app
-# resource "kubernetes_deployment" "express_app" {
-#   metadata {
-#     name      = "express-app-deployment"
-#     namespace = "default"
-#   }
-#   spec {
-#     replicas = 2
-#     selector {
-#       match_labels = {
-#         app = "express-app"
-#       }
-#     }
-#     template {
-#       metadata {
-#         labels = {
-#           app = "express-app"
-#         }
-#       }
-#       spec {
-#         container {
-#           name  = "express-app"
-#           image = "${aws_ecr_repository.express_app_repo.repository_url}:latest" # Replace with your actual Docker image name
-#           port {
-#             container_port = 8080
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
+  endpoint_public_access  = true   # allow API access from internet
+  endpoint_private_access = true 
 
-# # Kubernetes Service for the Express app
-# resource "kubernetes_service" "express_app_service" {
-#   metadata {
-#     name      = "express-app-service"
-#     namespace = "default"
-#   }
-#   spec {
-#     selector = {
-#       app = "express-app"
-#     }
-#     port {
-#       port        = 80
-#       target_port = 8080
-#     }
-#     type = "LoadBalancer"
-#   }
-# }
+  name  = "my-express-app-eks"
+  kubernetes_version = "1.29"
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.public_subnets
+  control_plane_subnet_ids = module.vpc.public_subnets
+
+  # Configure the managed node group
+  eks_managed_node_groups = {
+    express_app_nodes = {
+      min_size       = 1
+      max_size       = 2 # Changed max_size to allow for scaling
+      desired_size   = 1
+      instance_types = ["t3.medium"]
+    }
+  }
+
+  tags = {
+    "project" = "ci-cd-eks"
+  }
+}
+
+data "aws_eks_cluster" "this" {
+  name = module.eks.cluster_name
+}
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+}
+
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
+
+}
+
+# Kubernetes Deployment for the Express app
+resource "kubernetes_deployment" "express_app" {
+  metadata {
+    name      = "express-app-deployment"
+    namespace = "default"
+  }
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = "express-app"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "express-app"
+        }
+      }
+      spec {
+        container {
+          name  = "express-app"
+          image = "${aws_ecr_repository.express_app_repo.repository_url}:latest" # Replace with your actual Docker image name
+          port {
+            container_port = 8080
+          }
+        }
+      }
+    }
+  }
+}
+
+# Kubernetes Service for the Express app
+resource "kubernetes_service" "express_app_service" {
+  metadata {
+    name      = "express-app-service"
+    namespace = "default"
+  }
+  spec {
+    selector = {
+      app = "express-app"
+    }
+    port {
+      port        = 80
+      target_port = 8080
+    }
+    type = "LoadBalancer"
+  }
+}
 
 # add ecr repo to store the images we create
 
