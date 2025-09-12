b31c78b94526a746ba3a7a20d05da08d65e74e7f
fitsum <belay.fitsum@gmail.com>
2025-09-12 11:40:21 +0300

    running tf with temporary assumed role to deploy EKS1.2

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -60,7 +60,7 @@ module "eks" {
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.this.token
+  token                  = data.aws_eks_cluster_token
 
 }
 
