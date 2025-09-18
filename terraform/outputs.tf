8368e6aed422f6f2906d24dcbfe80a2fd784851c
fitsum <belay.fitsum@gmail.com>
2025-09-19 00:03:14 +0300

    make use of outputs and variables alot1.2

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,15 @@
-output "cd_user_access_key_id" {
-  description = "AWS key ID for CD user"
-  value       = aws_iam_access_key.cd
-  sensitive   = true
-}
+# output "cd_user_access_key_id" {
+#   description = "AWS key ID for CD user"
+#   value       = aws_iam_access_key.cd
+#   sensitive   = true
+# }
 
-output "cd_user_access_key_secret" {
-  description = "Access Key secret for CD user"
-  value       = aws_iam_access_key.cd.secret
-  sensitive   = true
+# output "cd_user_access_key_secret" {
+#   description = "Access Key secret for CD user"
+#   value       = aws_iam_access_key.cd.secret
+#   sensitive   = true
 
-}
+# }
 
 output "cluster_platform_version" {
   description = "Platform version for the cluster"
