e1e59b8c50451255442b950650758ce351e1390d
fitsum <belay.fitsum@gmail.com>
2026-08-28 22:47:37 +0300

    building new image for test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send(process.env.APP_MESSAGE || 'Hello from Express!');
+    res.send(process.env.APP_MESSAGE || 'Hello from Express!- Gitops with FluxCD');
 });
 
 app.listen(8080, () => {
