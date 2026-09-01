ff8c36f6c69c49dea0bb0cdbb43415e88853c1e3
fitsum <belay.fitsum@gmail.com>
2026-09-01 21:46:04 +0300

    update message for v1.0.1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send(process.env.APP_MESSAGE || 'Hello from Express!- Gitops with FluxCD');
+    res.send(process.env.APP_MESSAGE || 'Hello from simpleweb v1.0.1!');
 });
 
 app.listen(8080, () => {
